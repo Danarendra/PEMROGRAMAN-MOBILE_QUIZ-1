c954b94478689848e4a9be76cd8072c87ba1b0ab
Danarendra <narendra.daniswara16@gmail.com>
2026-03-11 09:23:01 +0700

    quiz

---
+++ Quiz1/src/kuis_244107060095/bin/kuis_244107060095.dart
@@ -0,0 +1,5 @@
+import 'package:kuis_244107060095/kuis_244107060095.dart' as kuis_244107060095;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kuis_244107060095.calculate()}!');
+}
---
+++ Quiz1/src/kuis_244107060095/test/kuis_244107060095_test.dart
@@ -0,0 +1,8 @@
+import 'package:kuis_244107060095/kuis_244107060095.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
