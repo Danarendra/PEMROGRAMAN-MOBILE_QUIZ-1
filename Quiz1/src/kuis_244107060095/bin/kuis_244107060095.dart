e94cc8e551a7f9b21d93250e0a5cf5d2b077956f
Danarendra <narendra.daniswara16@gmail.com>
2026-03-11 09:27:19 +0700

    personalisasi nim

--- Quiz1/src/kuis_244107060095/bin/kuis_244107060095.dart
+++ Quiz1/src/kuis_244107060095/bin/kuis_244107060095.dart
@@ -1,1 +1,18 @@
-import 'package:kuis_244107060095/kuis_244107060095.dart' as kuis_244107060095;
+import 'package:kuis_244107060095/kuis_244107060095.dart';
+
+void main() {
+  String nama = "Nama Anda";
+  String nim = "1234567890";
+
+  double nilaiUnikNIM = 890;
+
+  List<double> hargaBarang = [50000.5, 75000.75, 20000.25, 45000.0, 30000.5];
+
+  hargaBarang.add(nilaiUnikNIM);
+
+  double totalAwal = hitungTotal(hargaBarang);
+
+  print("Nama: $nama");
+  print("NIM: $nim");
+  print("Total Awal: Rp$totalAwal");
+}
